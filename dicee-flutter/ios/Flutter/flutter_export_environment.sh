b0be370deb0a0a2068d5d4088245955a4bc79321
nikita <freenikita95@gmail.com>
2021-02-02 23:55:46 +0200

    update

--- dicee-flutter/ios/Flutter/flutter_export_environment.sh
+++ dicee-flutter/ios/Flutter/flutter_export_environment.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=/Users/nikita/SDK/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/nikita/Projects/dicee-flutter"
-export "FLUTTER_TARGET=/Users/nikita/Projects/dicee-flutter/lib/main.dart"
+export "FLUTTER_APPLICATION_PATH=/Users/nikita/Projects/basic_flutter/dicee-flutter"
+export "FLUTTER_TARGET=/Users/nikita/Projects/basic_flutter/dicee-flutter/lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build/ios"
 export "OTHER_LDFLAGS=$(inherited) -framework Flutter"
